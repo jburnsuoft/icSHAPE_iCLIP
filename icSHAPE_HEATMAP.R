bdb5b75a715e003f3a5263973149e2a3d2fc1e3e
jburnsuoft <87782298+jburnsuoft@users.noreply.github.com>
2022-11-08 18:49:51 -0500

    Update icSHAPE_HEATMAP.R

--- icSHAPE_HEATMAP.R
+++ icSHAPE_HEATMAP.R
@@ -7,9 +7,9 @@ library(mclust)
 library(pheatmap)
 library(apcluster)
 
-setwd("~/icSHAPE_Project")
-vitroShapeFolder <- "SRSF1_A_100_SHAPE_VITRO_20_0//"
-vivoShapeFolder <- "SRSF1_A_100_SHAPE_VIVO_20_0//"
+#Define icSHAPE Data Folders
+#vitroShapeFolder <- "SRSF1_A_100_SHAPE_VITRO"
+#vivoShapeFolder <- "SRSF1_A_100_SHAPE_VIVO"
 
 loadShapeFile <- function(shapePath) {
   shape <- read.table(shapePath, 
@@ -81,19 +81,17 @@ save_pheatmap_pdf <- function(x, filename, width=7, height=7) {
   dev.off()
 }
 
+combineCols <- function(shapeDfs) {
+  shape <- data.frame()
+  for (item in shapeDfs) {
+    shape <- rbind(shape, item$"V2")
+  }
+  nums <- 1:length(shapeDfs)
+  rownames(shape) <- 
+    return(shape)
+}
 
-
-
-HEATMAP <- pheatmap(data.matrix(shapediffs), clustering_distance_rows = "euclidean", clustering_method = "complete", cluster_cols = FALSE, fontsize_row = 10, kmeans_k = 20, cutree_rows = 2, main = vitroShapeFolder)
-SHORTMAP <- pheatmap(data.matrix(shortshape), clustering_distance_rows = "euclidean", clustering_method = "complete", cluster_cols = FALSE, fontsize_row = 20, kmeans_k = 3, cutree_rows = 2, main = vitroShapeFolder)
-CLUSTERDF <- as.data.frame(HEATMAP$kmeans$cluster, header = F )
-SHORTCLUSTERDF <- as.data.frame(SHORTMAP$kmeans$cluster, header = F )
-HEATMAP <- pheatmap(data.matrix(shapediffs), cluster_cols = FALSE, fontsize_row = 10)
-save_pheatmap_pdf(HEATMAP, paste(vitroShapeFolder, "CLUSTMAP_NCLUST_", length(HEATMAP$tree_row$labels), ".pdf", sep = ""))
-save_pheatmap_pdf(SHORTMAP, paste(vitroShapeFolder, "SHORTCLUSTMAP_DIFF", ".pdf", sep = ""))
-write.table(HEATMAP$kmeans$cluster, file = paste(vitroShapeFolder, "KMEANSCLUST.txt", sep = ""), quote = FALSE, sep = '\t' )
-write.table(SHORTMAP$kmeans$cluster, file = paste(vitroShapeFolder, "SHORT_KMEANSCLUST.txt", sep = ""), quote = FALSE, sep = '\t' )
-heatmap(data.matrix(shapediffs), Rowv = FALSE, Colv = TRUE, )
+#####DETERMINE CLUSTERING PARAMETERS#####
 
 #ELBOW CLUSTERING
 set.seed(13)
@@ -133,59 +131,3 @@ set.seed(13)
 d.apclus2 <- apcluster(negDistMat(r=2), shapediffs)
 cat("affinity propogation optimal number of clusters:", length(d.apclus2@clusters), "\n")
 heatmap(d.apclus2)
-
-
-
-
-#ELBOW CLUSTERING
-set.seed(13)
-wss <- (nrow(shortshape))*sum(apply(shortshape,2,var))
-for (i in 2:15) wss[i] <- sum(kmeans(shortshape,
-                                     centers=i)$withinss)
-plot(1:15, wss, type="b", xlab="Number of Clusters",
-     ylab="Within groups sum of squares")
-
-#SILHOUETTE ANALYSIS
-pamk.best2 <- pamk(shortshape)
-cat("number of clusters estimated by optimum average silhouette width:", pamk.best2$nc, "\n")
-plot(pam(shortshape, pamk.best2$nc))
-
-#GAP STATISTICS
-clusGap(shortshape, kmeans, 10, B = 100, verbose = interactive())
-
-#HIERARCHICAL CLUSTERING
-shortshape_dist <- dist(as.matrix(shortshape))   # find distance matrix
-plot(hclust(shortshape_dist))
-
-#CALINSKY CRITERION
-cal_fit2 <- cascadeKM(shortshape, 1, 10, iter = 1000)
-plot(cal_fit2, sortg = TRUE, grpmts.plot = TRUE)
-calinski.best2 <- as.numeric(which.max(cal_fit2$results[2,]))
-cat("Calinski criterion optimal number of clusters:", calinski.best2, "\n")
-
-#BAYESIAN - MAXIMIZATION
-d_clust2 <- Mclust(as.matrix(shortshape), G=1:20)
-m.best2 <- dim(d_clust2$z)[2]
-cat("model-based optimal number of clusters:", m.best2, "\n")
-plot(d_clust2)
-
-
-#HEIRARCHICAL CLUSTERING
-set.seed(13)
-d.apclus2 <- apcluster(negDistMat(r=2), shortshape)
-cat("affinity propogation optimal number of clusters:", length(d.apclus2@clusters), "\n")
-heatmap(d.apclus2)
-
-combineCols <- function(shapeDfs) {
-  shape <- data.frame()
-  for (item in shapeDfs) {
-    shape <- rbind(shape, item$"V2")
-  }
-  nums <- 1:length(shapeDfs)
-  rownames(shape) <- 
-    return(shape)
-}
-
-shapeav <- combineCols(shapediffs)
-
-write.table(shapediffs, file = "SHAPEDIFFS.txt", quote = FALSE, sep = '\t')
